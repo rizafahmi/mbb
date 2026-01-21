8fb8d3b80653643fa08374101855812f29ef6c88
Riza Fahmi <rizafahmi@gmail.com>
2026-01-21 19:10:30 +0700

    Setup escript

--- lib/mbb.ex
+++ lib/mbb.ex
@@ -1,18 +1,5 @@
 defmodule Mbb do
-  @moduledoc """
-  Documentation for `Mbb`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Mbb.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def main(args) do
+    IO.puts("Hello, world!")
   end
 end
