bd8deb4a82d899dc07b7ebbd6aa7198dbff7e8f5
Riza Fahmi <rizafahmi@gmail.com>
2026-01-21 19:07:13 +0700

    Init

---
+++ lib/mbb.ex
@@ -0,0 +1,18 @@
+defmodule Mbb do
+  @moduledoc """
+  Documentation for `Mbb`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Mbb.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/mbb_test.exs
@@ -0,0 +1,8 @@
+defmodule MbbTest do
+  use ExUnit.Case
+  doctest Mbb
+
+  test "greets the world" do
+    assert Mbb.hello() == :world
+  end
+end
