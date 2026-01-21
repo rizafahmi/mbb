05bfe039cc05faf2f280175b3253f79057274d0c
Riza Fahmi <rizafahmi@gmail.com>
2026-01-21 20:27:32 +0700

    feat: request to cloud llm

--- lib/mbb.ex
+++ lib/mbb.ex
@@ -1,7 +1,45 @@
 defmodule Mbb do
+  def send(message) do
+    req =
+      Req.new(base_url: "https://api.anthropic.com/v1/messages")
+      |> Req.Request.put_header("Content-Type", "application/json")
+      |> Req.Request.put_header("x-api-key", System.get_env("API_KEY"))
+      |> Req.Request.put_header("anthropic-version", "2023-06-01")
+      |> Req.post(
+        json: %{
+          "model" => "claude-haiku-4-5-20251001",
+          "max_tokens" => 10000,
+          "temperature" => 1,
+          "system" =>
+            "You are an excellent principal engineer. You love programming language. Your favorite language is Elixir and you always write code in functional paradigm. You always answer in a concise and precise manner.",
+          "messages" => [
+            %{"role" => "user", "content" => message}
+          ]
+        }
+      )
+
+    case req do
+      {:ok, response} ->
+        text = response.body["content"] |> List.first() |> Map.get("text")
+        {:ok, text}
+
+      {:error, error} ->
+        {:error, error}
+    end
+  end
+
   def main(args, system_mod \\ System)
 
-    IO.puts("Hello, world!")
+  def main([args], system_mod) do
+    case send(args) do
+      {:ok, response} ->
+        IO.puts(response)
+        system_mod.halt(0)
+
+      {:error, error} ->
+        IO.puts("Error: #{inspect(error)}")
+        system_mod.halt(1)
+    end
   end
 
   def main([], system_mod) do
--- test/mbb_test.exs
+++ test/mbb_test.exs
@@ -3,10 +3,28 @@ defmodule MbbTest do
   doctest Mbb
   import ExUnit.CaptureIO
 
-  test "greets the world" do
-    assert Mbb.hello() == :world
-  test "main/1 should print help message if no args" do
-    result = capture_io(fn -> Mbb.main([], SystemMock) end)
-    assert result == "Usage: ./mbb <your question>\n"
+  @prompt "How many r in Strawberry? Answer with one following: one, two, three"
+
+  describe "main/1 function" do
+    test "main/1 should return string from llm if everything correct" do
+      result = capture_io(fn -> Mbb.main([@prompt], SystemMock) end)
+      assert String.contains?(result, "three")
+    end
+
+    test "main/1 should return error if something wrong"
+
+    test "main/1 should print help message if no args" do
+      result = capture_io(fn -> Mbb.main([], SystemMock) end)
+      assert result == "Usage: ./mbb <your question>\n"
+    end
+  end
+
+  describe "send function" do
+    test "send message to llm and get response" do
+      {:ok, result} =
+        Mbb.send(@prompt)
+
+      assert String.contains?(result, "three") or String.contains?(result, "two")
+    end
   end
 end
