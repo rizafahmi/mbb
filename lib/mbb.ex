c148c20b807380c05ac42ee5cb8bd0850992bd5b
Riza Fahmi <rizafahmi@gmail.com>
2026-01-21 19:27:56 +0700

    feat: help function

--- lib/mbb.ex
+++ lib/mbb.ex
@@ -2,4 +2,7 @@ defmodule Mbb do
   def main(args) do
     IO.puts("Hello, world!")
   end
+  def main([]) do
+    IO.puts("Usage: ./mbb <your question>")
+  end
 end
--- test/mbb_test.exs
+++ test/mbb_test.exs
@@ -1,8 +1,12 @@
 defmodule MbbTest do
   use ExUnit.Case
   doctest Mbb
+  import ExUnit.CaptureIO
 
   test "greets the world" do
     assert Mbb.hello() == :world
+  test "main/1 should print help message if no args" do
+    result = capture_io(fn -> Mbb.main([]) end)
+    assert result == "Usage: ./mbb <your question>\n"
   end
 end
